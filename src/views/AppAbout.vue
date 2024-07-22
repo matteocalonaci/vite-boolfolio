29a2085c6e0ec8cc89636115ac7f76cd4a08e4f0
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-07-22 16:06:32 +0200

    Popolazione della vista About

--- src/views/AppAbout.vue
+++ src/views/AppAbout.vue
@@ -1,7 +1,7 @@
 <!-- JS -->
 <script>
 export default {
-    name: "AppContact",
+    name: "AppAbout",
     data() {
         return {
            
@@ -15,12 +15,46 @@ export default {
 
 <!-- HTML -->
 <template>
-Contact
+<h1>About Me</h1>
+
+<div class="card">
+    <col-5>
+    <p>
+        Mi presento, sono Matteo Calonaci sono nato l'8/10/1994 sono di Firenze e sono colui che ha realizzato questo progetto.
+        Sono un appassionato di tecnologia e di informatica, e ho deciso
+        di realizzare questo sito per poter mostrare le mie competenze e le mie conoscenze
+        in ambito tecnologico apprese durante questi mesi del cordo di Boolean.
+    </p>
+</col-5>
+<col-6 class="d-flex justify-content-center align-items-center m-0">
+<div class="card-img">
+<img src="../assets/kira_ed_io.jpg" alt="">
+</div>
+</col-6>
+</div>
 
 </template>
 
 
 <!-- CSS -->
 <style scoped>
+.card {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    }
+
+.card-img{
+    width: 20rem;
+    height: 20rem;
+    object-fit: cover;
+}
+
+img{
+    border-radius: 100%;
+    width: 100%;
+
+}
 
 </style>
