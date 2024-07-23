97f87cdf5872ebc1a29024bf90341194515e301d
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-07-23 19:01:01 +0200

    Avviso di errore se non rispetti i parametri prestabiliti

--- src/views/AppContact.vue
+++ src/views/AppContact.vue
@@ -9,15 +9,15 @@ export default {
             name: '',
             email: '',
             message: '',
-            errors: '',
+            errors: {},
             success: false
         }
     },
-    methods: {      
+    methods: {
         sendMessage() {
 
             //preparare la richiesta
-            const data = 
+            const data =
             {
                 name: this.name,
                 email: this.email,
@@ -27,10 +27,26 @@ export default {
 
             //chiamata axios
             axios
-            .post('http://localhost:8000/api/lead', data)
-            .then(response => {
-                console.log(response);
-            })
+                .post('http://localhost:8000/api/lead', data)
+                .then(response => {
+                    console.log(response);
+
+                    if (response.data.success) {
+                        //mostra un messaggio di success
+                        this.success = true;
+                        this.errors = null;
+
+                        this.name = '';
+                        this.email = '';
+                        this.message = '';
+
+                    } else {
+                        //mostra un messaggio di errore di validazione
+                        this.errors = false
+                        this.errors = response.data.errors
+                    }
+
+                })
 
             //gestire il response
         }
@@ -49,30 +65,51 @@ export default {
             <p>It’s important to note that the contact page is not the only place where you
                 should include contact information. You should also include a contact page on your website’s footer, as
                 well as on your About page. This way, visitors can easily find your contact information no matter
-                where they are on your site.</p>
+                where they are on your site.
+            </p>
+            <template v-if="success">
+                <div class="alert alert-success" role="alert">
+                    <b>Messaggio inviato</b>
+                </div>
+            </template>
+
         </div>
+
         <div class="col-6 p-4">
             <form @submit.prevent="sendMessage()">
                 <div class="mb-3">
                     <label for="name" class="form-label">Name</label>
-                    <input type="text" class="form-control" name="name" id="name" aria-describedby="helpId"
+                    <input type="text" class="form-control" :class="{'is-invalid': errors.name}" name="name" id="name" aria-describedby="helpId"
                         placeholder="" v-model="name">
+
+                        <p class="text-danger" v-for="(error, index) in errors.name" :key="`name-error-${index}`">
+                            {{ error }}
+                        </p>
 
                     <small id="nameHelper" class="form-text text-muted">Write your name</small>
                 </div>
                 <div class="mb-3">
                     <label for="email" class="form-label">Email</label>
-                    <input type="email" class="form-control" name="email" id="email" aria-describedby="helpId"
+                    <input type="text" class="form-control" :class="{'is-invalid': errors.email}" name="email" id="email" aria-describedby="emailHelp"
                         placeholder="" v-model="email">
+
+                        <p class="text-danger" v-for="(error, index) in errors.email" :key="`email-error-${index}`">
+                            {{ error }}
+                        </p>
+
+
 
                     <small id="emailHelper" class="form-text text-muted">Write your email</small>
                 </div>
                 <div class="mb-3">
                     <label for="message" class="form-label">Message</label>
-                    <textarea type="text" class="form-control" name="message" id="message" rows="3" placeholder=""
+                    <textarea type="text" class="form-control" :class="{'is-invalid': errors.message}" name="message" id="message" rows="3" placeholder=""
                         v-model="message">
-
                     </textarea>
+
+                    <p class="text-danger" v-for="(error, index) in errors.message" :key="`message-error-${index}`">
+                            {{ error }}
+                        </p>
 
                     <small id="messageHelper" class="form-text text-muted">Write your email text</small>
                 </div>
